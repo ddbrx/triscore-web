65676adbb9e56bca82721533be99856d12430dcb
Dmitry Dobrynin <dima@aimtech.com>
2020-04-13 12:19:45 +0300

    Fix mobile views v1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@ DATETIME=`date +%Y-%m-%d-%H-%M-%S`
 SOURCE='dist/triscore-app/./'
 DESTINATION='u1000367@37.140.192.78:/var/www/u1000367/data/triscore-versions/triscore-web-'$DATETIME'-'$GIT_COMMIT
 
-echo ===> Builing prod version
+echo "===> Building prod version"
 ng build --prod
 
-echo ===> Copy from $SOURCE to $DESTINATION
+echo "===> Copy from $SOURCE to $DESTINATION"
 rsync -aR $SOURCE $DESTINATION
