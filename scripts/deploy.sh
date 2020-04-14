5d9b81c8b35c7527d48fbd3e863c260c15d1a02f
Dmitry Dobrynin <dima@aimtech.com>
2020-04-14 11:53:09 +0300

    Uncomment deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ SOURCE='dist/triscore-app/./'
 DESTINATION='triscore-www@triscore-xen-regru:/var/www/triscore-www/data/available/triscore.me/'$DATETIME'-'$GIT_COMMIT
 
 echo "===> Building prod version"
-# ng build --prod
+ng build --prod
 
 echo "===> Copy from $SOURCE to $DESTINATION"
 rsync -aR $SOURCE $DESTINATION
